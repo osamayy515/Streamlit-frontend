f4e4dd51020ebe5874ddec1eec283362d63323a3
Osama saleem <osamasaleem515@gmail.com>
2022-11-02 13:21:03 +0500

    Adding markdown, col & collecting user input

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ with header:
     st.title('Welcome To My Data Science Project!')
     st.text('In this project I look the transactions of taxi in NYC. ...')
 
+st.cache(suppress_st_warning=True)
 with dataset:
     st.header('NYC taxi dataset')
     st.text('I found this dataset on https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page')
@@ -24,8 +25,19 @@ with dataset:
 with features:
     st.header('The features I created')
 
+    st.markdown('* **First feature:** I created this feature because of this... I calculated it using this logic')
+    st.markdown('* **Second feature:** I created this feature because of this... I calculated it using this logic')
+
 
 with model_training:
     st.header('Time to train the model!')
     st.text('Here you get to choose the hyperparameters of the model and see how the performance changes!')
 
+    sel_col, disp_col = st.columns(2)
+
+    max_depth = sel_col.slider('What should be the max depth of the model?', min_value=10, max_value=100, value=20, step=10)
+
+    n_estimators = sel_col.selectbox('How many tree should there be?', options=[100,200,300,'No limit'], index=0)
+
+    input_feature = sel_col.text_input('Which feature should be used as the input feature?', 'PULocationID')
+    
